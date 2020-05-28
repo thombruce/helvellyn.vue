c4a26470f6f5917653af1accd675c1658764fbfa
Thom Bruce <thom@thombruce.com>
2020-05-28 23:44:14 +0100

    add description to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
   v-layout(column justify-center align-center)
     v-container
-      h2 Welcome to {{ title }}
+      h2.display-1 Welcome to {{ title }}
+      p.headline {{ description }}
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: process.env.siteTitle
+      title: process.env.siteTitle,
+      description: process.env.siteDescription
     }
   }
 }
